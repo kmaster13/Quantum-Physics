5d73b350443b30b721b352bd9952e39d7570db95
Alexey <amaklov2002@gmail.com>
2023-01-27 23:18:35 +0400

    добавлен текст для атома водорода

--- src/main/java/com/ssau/tk/quantumphysics/secondLab/controllers/HydrogenAtomEllipseController.java
+++ src/main/java/com/ssau/tk/quantumphysics/secondLab/controllers/HydrogenAtomEllipseController.java
@@ -1,5 +1,16 @@
 package com.ssau.tk.quantumphysics.secondLab.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+import static com.ssau.tk.quantumphysics.SecondLabControllerUtils.setWindow;
+
 public class HydrogenAtomEllipseController {
 
+    @FXML
+    protected void onMenuButtonClick(ActionEvent event) throws IOException {
+        setWindow("menu2.fxml", event, "Меню");
+    }
 }
